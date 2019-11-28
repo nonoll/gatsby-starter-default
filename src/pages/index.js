a66734e55e51194b05803797db4671891bb53814
nonoll <hkroh@wemakeprice.com>
2019-11-28 17:08:51 +0900

    feat: AdSense add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
+import AdSense from "react-adsense";
+
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
 
 const IndexPage = () => (
   <Layout>
@@ -15,6 +18,13 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <AdSense.Google
+      client="ca-pub-6583782974868046"
+      slot="3185903488"
+      style={{ display: "block" }}
+      format="auto"
+      responsive="true"
+    />
   </Layout>
 )
 
