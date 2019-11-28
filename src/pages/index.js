e8d161c0ed75e8a520623d2d32befeec47cd11ae
nonoll <hkroh@wemakeprice.com>
2019-11-28 17:32:10 +0900

    feat: AdSese add #2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import AdSense from "react-adsense";
 
 import { Link } from "gatsby"
@@ -8,24 +8,42 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-    <AdSense.Google
-      client="ca-pub-6583782974868046"
-      slot="3185903488"
-      style={{ display: "block" }}
-      format="auto"
-      responsive="true"
-    />
-  </Layout>
-)
+const IndexPage = () => {
+
+  const insertAdSense = () => {
+    const script = document.createElement('script');
+    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+    document.body.appendChild(script);
+
+    (window.adsbygoogle = window.adsbygoogle || []).push({
+      google_ad_client: "ca-pub-6583782974868046",
+      enable_page_level_ads: true
+    });
+  }
+
+  useEffect(() => {
+    insertAdSense();
+  }, []);
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+      <AdSense.Google
+        client="ca-pub-6583782974868046"
+        slot="3185903488"
+        style={{ display: "block" }}
+        format="auto"
+        responsive="true"
+      />
+    </Layout>
+  );
+}
 
 export default IndexPage
